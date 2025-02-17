b4a5961b0b880fa5e932d3932230490f2f7203a6
Axel Leroy <axeleroy@users.noreply.github.com>
2025-02-17 18:46:05 +0100

    Install Sunshine

--- build.sh
+++ build.sh
@@ -9,10 +9,5 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-dnf config-manager setopt fedora-cisco-openh264.enabled=1
-
-dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
-dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-
-# dnf install steam -y
-dnf install gamescope -y
+dnf copr enable lizardbyte/stable
+dnf install Sunshine
